7c5e76d6a46f3d2027da44e87cea7bb114e6eea0
Sandra LE THIEC <lethiec.sandra@gmail.com>
2017-11-23 15:02:13 +0100

    ajout profil DEV et profil PROD

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,18 +1,21 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
 
-    	String asciiArt = FigletFont.convertOneLine("Projet Maven");
-        System.out.println(asciiArt);
-//        System.out.println( "Hello World!" );
-    }
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
 }
